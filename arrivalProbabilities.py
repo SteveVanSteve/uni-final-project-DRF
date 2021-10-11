5508a2d87b2110cbec808fccff2c05761f13988e
Steve Shields <stevie.shields@hotmail.co.uk>
2021-10-11 18:07:05 +0100

    4th update 11/10/2021

--- arrivalProbabilities.py
+++ arrivalProbabilities.py
@@ -1,50 +1,57 @@
 from django.db import models
+import numpy
+import matplotlib.pyplot as pyplot
+import numpy_hist_io
 import csv
 import os
-import numpy
-import numpy_hist_io
 
 
 class ArrivalProbabilities(models.Model):
-    arrivalProbId = models.IntegerField(
-        primary_key=True), (_("Arrival Probability ID"))
-    binEdge = models.FloatField(null=False), (_("Bin Edge"))
-    binEntry = models.FloatField(null=True), (_("Bin Entry"))
-    hist = (numpy.array([]), numpy.array([]))
+    arrivalProbId = models.IntegerField(primary_key=True)
+    binEdge = models.FloatField(null=False)
+    binEntry = models.FloatField(null=True)
 
-    class Meta:
-        delimiter = ","
+    # # Create some bin edges.
+    bin_edges = []
+    offset = 4.5
+    for i in range(6):
+        bin_edges.append(offset+i)
+
+# Add some entries.
+# Update these are percentages for probability times - in preparation for the charging times
+    n_bins = len(bin_edges)-1
+    bin_entries = [0.]*n_bins
+    bin_entries[0] = 10  # 4.30 to 5.30pm
+    bin_entries[1] = 27  # 5.30 to 6.30pm
+    bin_entries[2] = 36  # 6.30 to 7.30pm
+    bin_entries[3] = 22  # 7.30 to 8.30pm
+    bin_entries[4] = 5  # 8.30 to 9.30pm
+    hist = (numpy.array(bin_entries), numpy.array(bin_edges))
 
 
-# import numpy
-# import numpy_hist_io
+def hist_from_csv(file_name: str):
+    """ 
+    A function to read a numpy histogram from a CSV file.
+    """
+    input_file = open(file_name, 'r', newline='')
+    csv_reader = csv.reader(input_file)
+    bin_entries = []
+    bin_edges = []
+    for row in csv_reader:
+        if csv_reader.line_num == 1:
+            if row[0] != 'bin entries' or row[1] != 'bin edges':
+                return None
+        try:
+            float_value = float(row[0])
+            bin_entries.append(float_value)
+        except ValueError:
+            pass
 
-# """
-# A program to demonstrate forming a numpy histogram from
-# data values.
-# """
+        try:
+            float_value = float(row[1])
+            bin_edges.append(float_value)
+        except ValueError:
+            pass
 
-# # Create some bin edges.
-# bin_edges = []
-# offset = 4.5
-# for i in range(6):
-#     bin_edges.append(offset+i)
-
-# # Add some entries.
-# # Update these are percentages for probability times - in preparation for the charging times
-# n_bins = len(bin_edges)-1
-# bin_entries = [0.]*n_bins
-# bin_entries[0] = 10  # 4.30 to 5.30pm
-# bin_entries[1] = 27  # 5.30 to 6.30pm
-# bin_entries[2] = 36  # 6.30 to 7.30pm
-# bin_entries[3] = 22  # 7.30 to 8.30pm
-# bin_entries[4] = 5  # 8.30 to 9.30pm
-
-# # Create the histogram data tuple.
-# hist = (numpy.array(bin_entries), numpy.array(bin_edges))
-
-# # Save the histogram as a PNG file.
-# numpy_hist_io.hist_to_plot(hist, "times.png")
-
-# # Save the histogram as a CSV file.
-# numpy_hist_io.hist_to_csv(hist, "times.csv")
+    input_file.close()
+    return (numpy.array(bin_entries), numpy.array(bin_edges))
--- simulation/models.py
+++ simulation/models.py
@@ -2,10 +2,11 @@ from django.db import models
 import numpy
 import numpy_hist_io
 import csv
+import os
 
 
 class ArrivalProbabilities(models.Model):
     arrivalProbId = models.IntegerField(primary_key=True)
     binEdge = models.FloatField(null=False)
     binEntry = models.FloatField(null=True)
-    hist = (numpy.array([]), numpy.array([]))
+    hist = (numpy.array(bin_entries), numpy.array(bin_edges))
