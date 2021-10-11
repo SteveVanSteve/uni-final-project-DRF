eb8c5d4c648a2c873b45154c6e0a1fa3f921dc5b
Steve Shields <stevie.shields@hotmail.co.uk>
2021-10-11 18:12:18 +0100

    5th update - testing out csv reading 11/10/2021

--- arrivalProbabilities.py
+++ arrivalProbabilities.py
@@ -29,6 +29,69 @@ class ArrivalProbabilities(models.Model):
     hist = (numpy.array(bin_entries), numpy.array(bin_edges))
 
 
+def hist_bin_centres(numpy_hist: tuple):
+    """
+    A function to return the bin centres from a numpy histogram.
+    """
+    bin_entries = numpy_hist[0]
+    bin_edges = numpy_hist[1]
+    n_bins = len(bin_entries)
+    bin_centres = []
+    bin_widths = []
+    for i in range(n_bins):
+        bin_width = bin_edges[i+1] - bin_edges[i]
+        bin_widths.append(bin_width)
+        bin_centre = bin_edges[i] + bin_width*0.5
+        bin_centres.append(bin_centre)
+    return (bin_centres, bin_widths)
+
+
+def normalised_entries(numpy_hist: tuple):
+    """
+    A function to return normalised entries of a numpy histogram.
+    """
+    bin_entries = []
+    bin_entries += list(numpy_hist[0])
+    total = sum(bin_entries)
+    for i in range(len(bin_entries)):
+        bin_entries[i] /= total
+    return bin_entries
+
+
+def hist_to_plot(numpy_hist: tuple, file_name: str):
+    """ 
+    A function to write a numpy histogram to a png file.
+    """
+    bin_entries = numpy_hist[0]
+    (bin_centres, bin_widths) = hist_bin_centres(numpy_hist)
+    pyplot.bar(bin_centres, bin_entries, bin_widths)
+    pyplot.savefig(file_name)
+    pyplot.close()
+
+
+def hist_to_csv(numpy_hist: tuple, file_name: str):
+    """ 
+    A function to write a numpy histogram to a CSV file.
+    """
+    output_file = open(file_name, 'w', newline='')
+    csv_writer = csv.writer(output_file)
+    csv_writer.writerow(['bin entries', 'bin edges'])
+
+    bin_entries = numpy_hist[0]
+    bin_edges = numpy_hist[1]
+    n_bins = len(bin_entries)
+
+    for i in range(n_bins+1):
+        if i == n_bins:
+            csv_writer.writerow(["", bin_edges[i]])
+            continue
+        csv_writer.writerow([bin_entries[i], bin_edges[i]])
+    output_file.close()
+
+# reading numpy histogram from csv - I need to do this but from a database - that database
+# could contain the pasted in csv values
+
+
 def hist_from_csv(file_name: str):
     """ 
     A function to read a numpy histogram from a CSV file.
