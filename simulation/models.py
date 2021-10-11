d1d99188e75e7e2459fdf68a39570b88ba8ab15d
Steve Shields <stevie.shields@hotmail.co.uk>
2021-10-11 15:15:17 +0100

    3rd update - initial model to gridproject_db

--- simulation/models.py
+++ simulation/models.py
@@ -1,13 +1,11 @@
 from django.db import models
-from django.utils.translation import gettext as _
 import numpy
 import numpy_hist_io
 import csv
 
 
 class ArrivalProbabilities(models.Model):
-    arrivalProbId = models.IntegerField(
-        primary_key=True), default = None, (_("Arrival Probability ID"))
-    binEdge = models.FloatField(null=False), (_("Bin Edge"))
-    binEntry = models.FloatField(null=True), (_("Bin Entry"))
+    arrivalProbId = models.IntegerField(primary_key=True)
+    binEdge = models.FloatField(null=False)
+    binEntry = models.FloatField(null=True)
     hist = (numpy.array([]), numpy.array([]))
