71f2c731e62b3fb032975db568a034c0ad15d3d6
Steve Shields <stevie.shields@hotmail.co.uk>
2021-10-14 19:14:57 +0100

    updates 14/10/2021

--- simulation/models.py
+++ simulation/models.py
@@ -7,6 +7,18 @@ import os
 
 class ArrivalProbabilities(models.Model):
     arrivalProbId = models.IntegerField(primary_key=True)
-    binEdge = models.FloatField(null=False)
     binEntry = models.FloatField(null=True)
-    hist = (numpy.array([bin_entries]), numpy.array(bin_edges))
+    binEdge = models.FloatField(null=False)
+    hist = (numpy.array(binEntry), numpy.array(binEdge))
+
+
+class BackgroundProfiles(models.Model):
+    BackgroundId = models.IntegerField(primary_key=True)
+    BackgroundName = models.CharField(max_length=200)
+
+
+class BackgroundPower(models.Model):
+    BackgroundPowerId = models.IntegerField(primary_key=True)
+    BackgroundId = models.ForeignKey
+    Time = models.FloatField(null=False)
+    Power = models.FloatField(null=False)
