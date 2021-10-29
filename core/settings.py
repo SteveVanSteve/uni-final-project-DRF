708c0d9f94e0d7e21ea7d28b3b7ca4bc84fe0ba7
Steve Shields <stevie.shields@hotmail.co.uk>
2021-10-29 17:44:47 +0100

    updates and edits - 29/10/2021

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'simulation',
     'simulation_api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- simulation/admin.py
+++ simulation/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from .models import ArrivalProbabilities
 
 admin.site.register(ArrivalProbabilities)
