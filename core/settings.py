05b97e477dc9b08cbe3ea479198776e40c5fa621
Steve Shields <stevie.shields@hotmail.co.uk>
2021-11-03 15:50:35 +0000

    edits

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'simulation',
+    'simulation.apps.SimulationConfig',
     'rest_framework',
     'background_task',
     'debug_toolbar',
--- simulation/serializers.py
+++ simulation/serializers.py
@@ -3,12 +3,54 @@ from simulation.models import ArrivalProbabilities, ChargingCurve
 
 
 class ArrivalProbabilitiesSerializer(serializers.HyperlinkedModelSerializer):
+    arrivalProbId = serializers.IntegerField(read_only=True)
+    binEntry = serializers.FloatField(read_only=True)
+    binEdge = serializers.FloatField(read_only=True)
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Arrival Probabilities` instance, given the validated data.
+        """
+        return ArrivalProbabilities.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return a new `Arrival Probabilities` instance, given the validated data.
+        """
+        instance.arrivalProbId = validated_data.get(
+            'arrivalProbId', instance.arrivalProbId)
+        instance.binEntry = validated_data.get('binEntry', instance.binEntry)
+        instance.binEdge = validated_data.get('binEdge', instance.binEdge)
+        instance.save()
+        return instance
+
     class Meta:
         model = ArrivalProbabilities
         fields = ['arrivalProbId', 'binEntry', 'binEdge']
 
 
 class ChargingCurveSerializer(serializers.HyperlinkedModelSerializer):
+    chargingCurveId = serializers.IntegerField(read_only=True)
+    Time = serializers.FloatField(read_only=True)
+    Power = serializers.FloatField(read_only=True)
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Charging Curve` instance, given the validated data.
+        """
+        return ChargingCurve.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return a new `Arrival Probabilities` instance, given the validated data.
+        """
+        instance.chargingCurveId = validated_data.get(
+            'chargingCurveId', instance.chargingCurveId)
+        instance.Time = validated_data.get('times', instance.Time)
+        instance.Power = validated_data.get('powers', instance.Power)
+        instance.save()
+        return instance
+
     class Meta:
         model = ChargingCurve
         fields = ['chargingCurveId', 'times', 'powers']
